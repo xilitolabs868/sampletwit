b9602565cc5a7217a20f0fc004cc0f94847198a7
xilitolabs868 <xilitol@hanmail.net>
2011-08-06 21:17:57 +0900

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -43,5 +43,6 @@ ActionController::Routing::Routes.draw do |map|
   map.contact '/contact', :controller => 'pages', :action => 'contact'
   map.about   '/about',   :controller => 'pages', :action => 'about'
   map.help    '/help',    :controller => 'pages', :action => 'help'
-  map.root                :controller => 'pages', :action => 'home'
+  map.signup  '/signup',  :controller => 'users', :action => 'new'
+  map.root :controller => 'pages', :action => 'home'
 end
